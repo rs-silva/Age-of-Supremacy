a0a5142872dcb37e5ae9ec32ba33ae84d4a66ac7
Rui Silva <NB28646@novabase.pt>
2023-08-28 14:24:19 +0000

    Created auth and database microservice.

---
+++ auth/src/main/java/org/example/controllers/AuthController.java
@@ -0,0 +1,14 @@
+package org.example.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public ResponseStatus<?>
+}
