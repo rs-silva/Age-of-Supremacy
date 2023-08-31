cc0a9f84f33a7b04361b333540a0ce98b73559d4
Rui Silva <NB28646@novabase.pt>
2023-08-31 00:12:05 +0000

    H2 is finally working.

--- auth/src/main/java/com/example/config/AuthConfig.java
+++ auth/src/main/java/com/example/config/AuthConfig.java
@@ -2,6 +2,7 @@ package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
--- database/src/main/java/com/example/config/SecurityConfig.java
+++ database/src/main/java/com/example/config/SecurityConfig.java
@@ -2,14 +2,15 @@ package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
@@ -23,27 +24,31 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests( (authorize) -> authorize
-                .requestMatchers(antMatcher("/")).permitAll()
-                .requestMatchers(antMatcher("/h2-console/*")).permitAll()
-                .requestMatchers(antMatcher("/h2-console/**")).permitAll()
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            //.csrf(csrf -> csrf.ignoringRequestMatchers(toH2Console()))
+            .headers(headers -> headers
+                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+            .authorizeHttpRequests(auth -> auth
+                //.requestMatchers(antMatcher("/")).permitAll()
+                .requestMatchers(toH2Console()).permitAll()
                 .requestMatchers(antMatcher("/api/users")).permitAll()
                 .requestMatchers(antMatcher("/api/users/*")).permitAll()
-                .anyRequest().authenticated()
-                );
-                /*.formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/", true)
-                .failureUrl("/login-error")
-                .permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .deleteCookies("JSESSIONID")
-                .and()
-                .exceptionHandling()
-                .accessDeniedPage("/denied");*/
+            .anyRequest().authenticated()
+            );
+
+            /*.formLogin()
+            .loginPage("/login")
+            .defaultSuccessUrl("/", true)
+            .failureUrl("/login-error")
+            .permitAll()
+            .and()
+            .logout()
+            .logoutSuccessUrl("/")
+            .deleteCookies("JSESSIONID")
+            .and()
+            .exceptionHandling()
+            .accessDeniedPage("/denied");*/
 
         return http.build();
     }
