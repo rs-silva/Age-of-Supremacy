55a0203f14ffbac84331d05e36bd7408b7d1866e
rs-silva <res.silva1998@gmail.com>
2023-11-14 12:59:21 -0100

    Added event-manager module.

---
+++ event-manager/src/main/java/org/example/EventManagerApplication.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+
+public class EventManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagerApplication.class, args);
+        System.out.println("Hello world!");
+    }
+}
