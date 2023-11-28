8958ae3df7606f374e3409b3aefb1c56cc212a3e
rs-silva <res.silva1998@gmail.com>
2023-11-28 16:34:01 -0100

    Added properties to Strategic Defense Center.

--- base-manager/src/main/java/com/example/services/buildings/DefenseCenterUtils.java
+++ base-manager/src/main/java/com/example/services/buildings/DefenseCenterUtils.java
@@ -46,17 +46,61 @@ public class DefenseCenterUtils implements BuildingUtils {
     public Map<String, String> getAdditionalProperties(Building building) {
         Map<String, String> properties = new HashMap<>();
 
-        int overallDefense = getOverallDefense(building.getLevel());
+        int overallDefense = getOverallDefense(building);
+        int antitankDefense = getAntitankDefense(building);
+        int aaDefense = getAADefense(building);
+
         properties.put(BuildingsPropertiesNames.DEFENSE_CENTER_OVERALL.getLabel(), String.valueOf(overallDefense));
+        properties.put(BuildingsPropertiesNames.DEFENSE_CENTER_ANTITANK.getLabel(), String.valueOf(antitankDefense));
+        properties.put(BuildingsPropertiesNames.DEFENSE_CENTER_AA.getLabel(), String.valueOf(aaDefense));
 
         return properties;
     }
 
-    private static int getOverallDefense(int level) {
-        return OVERALL_DEFENSE_VALUES[level - 1];
+    private static int getOverallDefense(Building building) {
+        int buildingLevel = building.getLevel();
+
+        int defense_factor = Integer.parseInt(building.getProperties().get(BuildingsPropertiesNames.DEFENSE_CENTER_OVERALL_FACTOR.getLabel()));
+        int base_defense = OVERALL_BASE_DEFENSE_VALUES[buildingLevel - 1];
+
+        return defense_factor * base_defense;
     }
 
-    private static final int[] OVERALL_DEFENSE_VALUES = { 10 , 20 , 30 , 40 , 50,
+    private static int getAntitankDefense(Building building) {
+        int buildingLevel = building.getLevel();
+
+        int defense_factor = Integer.parseInt(building.getProperties().get(BuildingsPropertiesNames.DEFENSE_CENTER_ANTITANK_FACTOR.getLabel()));
+        int base_defense = ANTITANK_BASE_DEFENSE_VALUES[buildingLevel - 1];
+
+        return defense_factor * base_defense;
+    }
+
+    private static int getAADefense(Building building) {
+        int buildingLevel = building.getLevel();
+
+        int defense_factor = Integer.parseInt(building.getProperties().get(BuildingsPropertiesNames.DEFENSE_CENTER_AA_FACTOR.getLabel()));
+        int base_defense = AA_BASE_DEFENSE_VALUES[buildingLevel - 1];
+
+        return defense_factor * base_defense;
+    }
+
+    private static final int[] OVERALL_BASE_DEFENSE_VALUES = { 10 , 20 , 30 , 40 , 50,
+                                                          60 , 70 , 80 , 90 , 100,
+                                                          110, 120, 130, 140, 150,
+                                                          160, 170, 180, 190, 200,
+                                                          210, 220, 230, 240, 250,
+                                                          260, 270, 280, 290, 300
+    };
+
+    private static final int[] ANTITANK_BASE_DEFENSE_VALUES = { 10 , 20 , 30 , 40 , 50,
+                                                                60 , 70 , 80 , 90 , 100,
+                                                                110, 120, 130, 140, 150,
+                                                                160, 170, 180, 190, 200,
+                                                                210, 220, 230, 240, 250,
+                                                                260, 270, 280, 290, 300
+    };
+
+    private static final int[] AA_BASE_DEFENSE_VALUES = { 10 , 20 , 30 , 40 , 50,
                                                           60 , 70 , 80 , 90 , 100,
                                                           110, 120, 130, 140, 150,
                                                           160, 170, 180, 190, 200,
