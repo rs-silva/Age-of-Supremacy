be2ad0bd68315f278457ffa821ae944c5e9a638c
rs-silva <res.silva1998@gmail.com>
2023-12-27 22:06:27 -0100

    Start of work on combat event.

---
+++ base-manager/src/main/java/com/example/utils/units/UnitsUtils.java
@@ -0,0 +1,20 @@
+package com.example.utils.units;
+
+import com.example.dto.ArmyDTO;
+import com.example.models.Base;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Component
+public class UnitsUtils {
+
+    public Timestamp calculateUnitsArrivalTime(Base originBase, Base destinationBase, ArmyDTO armyDTO) {
+        /* TODO calculate travelling time based on the bases' coordinates and the units' movement speed */
+        int travellingTimeInSeconds = 5;
+
+        return Timestamp.from(Instant.now().plusMillis(travellingTimeInSeconds * 1000));
+    }
+
+}
